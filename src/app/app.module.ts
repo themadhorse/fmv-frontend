299296ac3696592a133fe2fea5fe7f80af7def86
Harsh Kulkarni <harshshailkul@gmail.com>
2023-10-31 17:56:01 +0530

    Setting up firebase and angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"financial-market-visualizer","appId":"1:785271052222:web:0d589983f4782a40508bef","databaseURL":"https://financial-market-visualizer-default-rtdb.asia-southeast1.firebasedatabase.app","storageBucket":"financial-market-visualizer.appspot.com","apiKey":"AIzaSyAhGCElco9uZMwXPtGOyvdSawvwGXumoe8","authDomain":"financial-market-visualizer.firebaseapp.com","messagingSenderId":"785271052222","measurementId":"G-9Q2VP1QCTY"})),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
